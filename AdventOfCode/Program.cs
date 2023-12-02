cedb4df0628c5d0503c48246bac7f49d3b4b1bb5
Nokijoto <nokijoto@protonmail.com>
2023-12-02 19:13:28 +0100

    Day 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,6 +3,7 @@ using AdventOfCode2023.Days.Day1;
 using System;
 using System.IO;
 
+Console.WriteLine("Advent of Code 2023");
 
 Day1 Day1 = new Day1();
 Day1.FilePath = "C:\\Users\\Rexma\\Desktop\\AdventOfCode\\AdventOfCode\\AdventOfCode2023\\Data\\Day1\\input.txt";
